bfaaca10212b2185420f5a60a33e0ac6f3197580
J. Singleton <jennifer.singleton@gmail.com>
2019-03-23 02:54:12 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -21,7 +21,8 @@ def can_be_created_in_a_block(args = nil)
   # release_date == 1990
 
   Movie.create do |m|
-    __
+    m.title = args[title]
+    m.release_date = args[release_date]
   end
 end
 
