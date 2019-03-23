c2e1e9d5b77b616f3023ad0fb8f888542fd19356
J. Singleton <jennifer.singleton@gmail.com>
2019-03-23 03:29:24 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -17,10 +17,6 @@ def can_be_created_with_a_hash_of_attributes
 end
 
 def can_be_created_in_a_block(args = nil)
-  # If no arguments are passed, use default values:
-  # title == "Home Alone"
-  # release_date == 1990
-
   Movie.create do |m|
     args ? m.title = args[:title] : m.title = "Home Alone"
     args ? m.release_date = args[:release_date] : m.release_date = 1990
@@ -44,16 +40,10 @@ def can_find_the_first_item_from_the_database_using_id
 end
 
 def can_find_by_multiple_attributes
-  # Search Values:
-  # title == "Title"
-  # release_date == 2000
-  # director == "Me"
   Movie.find_by(title:"Title")
 end
 
 def can_find_using_where_clause_and_be_sorted
-  # For this test return all movies released after 2002 and ordered by
-  # release date descending
   Movie.where("release_date > 2002").order("release_date DESC")
 end
 
